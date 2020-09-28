112a0e7e056e3a2c07ebe8ef69a6437ec94c1487
Sherlock <aboutdh@lotte.net>
2020-09-28 18:06:15 +0900

    end lecture

--- server/index.js
+++ server/index.js
@@ -80,7 +80,7 @@ app.get("/api/users/auth", auth, (req, res) => {
   // 여기까지 미들웨어를 통과해 왔다는 얘기는 authentication 이 True 라는 말.
   res.status(200).json({
     _id: req.user._id,
-    isAdmin: req.user.role === 0 ? false : true,
+    isAdmin: req.user.role === 0 ? true : false,
     isAuth: true,
     email: req.user.email,
     name: req.user.name,
