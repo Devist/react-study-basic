a1451aa8368fc93880aa5dd552280d5c5b6483ea
Sherlock <aboutdh@lotte.net>
2020-11-05 17:20:19 +0900

    done.

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users"
+
+export const API_URL = "https://api.themoviedb.org/3/"
+export const API_KEY = "a5433c54e4c1a3d39760257918883fe4"
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/"
--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://odh:abcd1234@boilerplate.vjff7.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  mongoURI:
+    "mongodb+srv://odh:abcd1234@boilerplate.vjff7.mongodb.net/<dbname>?retryWrites=true&w=majority"
 }
